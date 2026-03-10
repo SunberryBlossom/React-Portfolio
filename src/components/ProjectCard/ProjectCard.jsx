1bd445743ff9e942894f80cc6bdb914cb082c122
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 16:10:43 +0100

    Add MVP ProjectCard component

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,12 @@
+
+export default function ProjectCard ({title, subtitle}) {
+    return (
+        <button>
+            <img />
+            <div>
+                <h2>{title}</h2>
+                <h3>{subtitle}</h3>
+            </div>
+        </button>
+    )
+}
