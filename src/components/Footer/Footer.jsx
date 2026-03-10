7f30f35ec2e4d973bccfbf53de1b806a79e5bcf6
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 14:49:52 +0100

    Add MVP Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+export default function Footer () {
+    return (
+        <footer classList={styles.footer}>
+            <p classList={styles.p}>
+                &amp 2026 Elvira Rebecca Mariesdotter. All rights reserved.
+            </p>
+        </footer>
+    )
+}
