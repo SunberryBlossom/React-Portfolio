c9bdba3c8c494e239561e46bbe789fefd3ee9ce3
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 16:39:17 +0100

    Add temporary default text

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.css';
 
-export default function Button ({ text }) {
+export default function Button ({ text ="default text" }) {
     return (
         <button className={styles.Button}>
             {text}
