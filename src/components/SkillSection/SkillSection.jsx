b32432e217af1e1c13c31dafdcd0acd7d779005f
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 15:03:48 +0100

    Add MVP SkillSection Component

---
+++ src/components/SkillSection/SkillSection.jsx
@@ -0,0 +1,22 @@
+import styles from './SkillSection.module.css';
+import SkillCard from '../SkillCard/SkillCard';
+
+export default function SkillSection () {
+    return (
+        <section classList={styles.SkillSection}>
+            <h2>My Current Skillset</h2>
+            <div>
+                <SkillCard skillName="HTML 5" />
+                <SkillCard skillName="CSS 3" />
+                <SkillCard skillName="JS" />
+                <SkillCard skillName="React" />
+                <SkillCard skillName="C# | .NET" />
+                <SkillCard skillName="MS SQL Server" />
+                <SkillCard skillName="PostGreSQL" />
+                <SkillCard skillName="Docker" />
+                <SkillCard skillName="Git VCS" />
+                <SkillCard skillName="REST API" />
+            </div>
+        </section>
+    )
+}
