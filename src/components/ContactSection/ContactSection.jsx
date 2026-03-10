3c7fed3801ad2d06307943e0aef91af22698e4c5
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 14:55:34 +0100

    Add MVP ContactSection Component

---
+++ src/components/ContactSection/ContactSection.jsx
@@ -0,0 +1,26 @@
+import styles from './ContactSection.module.css'
+import Button from '../Button/Button.jsx'
+
+export default function ContactSection () {
+    return (
+        <section classList={styles.section}>
+            <h2>
+                Get in touch with me!
+            </h2>
+            <div>
+                <Button
+                    text="Mail"
+                />
+                <Button
+                    text="LinkedIn"
+                />
+                <Button
+                    text="Phone"
+                />
+                <Button
+                    text="GitHub"
+                />
+            </div>
+        </section>
+    )
+}
