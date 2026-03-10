48c78b608b79897080aa6a0fa3dc9619e55ebdcf
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 16:00:14 +0100

    Add MVP AboutPage Component/view

---
+++ src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,16 @@
+import styles from './AboutPage.module.css';
+import Header from '../Header/Header.jsx'
+import AboutSection from '../AboutSection/AboutSection.jsx'
+import ExperienceSection from '../ExperienceSection/ExperienceSection.jsx'
+import Footer from '../Footer/Footer.jsx'
+
+export default function AboutPage () {
+    return (
+        <>
+        <Header classList={styles} />
+        <AboutSection />
+        <ExperienceSection />
+        <Footer />
+        </>
+    )
+}
