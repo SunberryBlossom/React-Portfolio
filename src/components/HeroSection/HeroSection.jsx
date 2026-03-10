e3da29908de16f1bfaf340345a52cdf3f8bed963
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-10 15:09:21 +0100

    Add MVP HeroSection Component

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,21 @@
+import styles from 'HeroSection.module.css';
+import Button from '../Button/Button.jsx'
+
+export default function HeroSection () {
+    return (
+        <section classList={styles.section}>
+            <div>
+                <img />
+            </div>
+            <h1>
+                Mae Govannen! Call me Vira
+            </h1>
+            <p>
+                A .NET fullstack system developer based in Stockholm, Sweden, with an obsession for data-intensive applications.
+            </p>
+            <Button
+                text="Let us work together!"
+            />
+        </section>
+    )
+}
