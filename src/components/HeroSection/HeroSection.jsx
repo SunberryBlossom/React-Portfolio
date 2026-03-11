89824cec9749a9f5e14da592d08d97fdd866718b
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 10:42:04 +0100

    Add static styling to HeroSection component and missing jsx tags

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,18 +4,22 @@ import Button from '../Button/Button.jsx'
 export default function HeroSection () {
     return (
         <section className={styles.section}>
-            <div>
-                <img src="src/assets/hero-image.png" />
+            <div className={styles.imageSection}>
+                <div className={styles.imageContainer}>
+                    <img className={styles.img} src="src/assets/hero-image.png" />
+                </div>
             </div>
-            <h1>
+            <div className={styles.content}>
+            <h1 className={styles.h1}>
                 Mae Govannen! Call me Vira
             </h1>
-            <p>
+            <p className={styles.p}>
                 A .NET fullstack system developer based in Stockholm, Sweden, with an obsession for data-intensive applications.
             </p>
             <Button
                 text="Let us work together!"
             />
+            </div>
         </section>
     )
 }
