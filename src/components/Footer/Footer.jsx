96307d9ae25709191d109aedfc6b33d4e77e12ac
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 14:55:56 +0100

    Add styling for Footer
    Added styling to the footer, including the ::before pseudo element.
    Also fixed so the same pseudo element for the header looked better by making it longer and not skewed from the left.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import styles from './Footer.module.css'
 export default function Footer () {
     return (
         <footer className={styles.footer}>
-            <p className={styles.p}>
-                &amp 2026 Elvira Rebecca Mariesdotter. All rights reserved.
+            <p>
+                © 2026 Elvira Rebecca Mariesdotter. All rights reserved.
             </p>
         </footer>
     )
