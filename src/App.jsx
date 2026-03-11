b65ac7579c1149cadd55a359e8b0249f76d13609
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 09:09:02 +0100

    Add global styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import AboutPage from "./components/AboutPage/AboutPage.jsx"
 import PortfolioPage from "./components/PortfolioPage/PortfolioPage.jsx"
 import ResumePage from "./components/ResumePage/ResumePage.jsx"
 import ContactPage from "./components/ContactPage/ContactPage.jsx"
+import './App.css'
 
 function App() {
   return (
     <HashRouter>
+      <a href="#main-content" className="skip-link">Skip to main content</a>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
