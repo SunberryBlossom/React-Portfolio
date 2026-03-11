c07833c2c2de47392e0d4798eddbb5a7732b0014
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 14:27:00 +0100

    Add styling to ContactSection component

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -4,20 +4,24 @@ import Button from '../Button/Button.jsx'
 export default function ContactSection () {
     return (
         <section className={styles.section}>
-            <h2>
+            <h2 className={styles.h2}>
                 Get in touch with me!
             </h2>
-            <div>
+            <div className={styles.container}>
                 <Button
+                    style={{width: '170px', padding: '10px'}}
                     text="Mail"
                 />
                 <Button
+                    style={{width: '170px', padding: '10px'}}
                     text="LinkedIn"
                 />
                 <Button
+                    style={{width: '170px', padding: '10px'}}
                     text="Phone"
                 />
                 <Button
+                    style={{width: '170px', padding: '10px'}}
                     text="GitHub"
                 />
             </div>
