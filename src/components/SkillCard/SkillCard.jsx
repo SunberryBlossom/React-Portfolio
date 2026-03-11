850f0bc490c962b5df766e07e4c0d60be90899ce
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 07:37:12 +0100

    Add functioning fontawesome icon to skill card component

--- src/components/SkillCard/SkillCard.jsx
+++ src/components/SkillCard/SkillCard.jsx
@@ -1,11 +1,20 @@
 import styles from './SkillCard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-export default function SkillCard ({skillName}) {
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
+export default function SkillCard ({skillName, icon, iconStyle}) {
     return (
         <div className={styles.div}>
             <h3 className={styles.h3}>
                 {skillName}
             </h3>
+            <FontAwesomeIcon icon={icon} style={iconStyle} />
         </div>
     )
 }
