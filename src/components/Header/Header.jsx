470431e61a3d9d62bf267e03e61c8e170d2ec048
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 10:24:42 +0100

    Transfer global styling spec. for Header component into component module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,14 @@ import styles from './Header.module.css';
 import Navbar from '../Navbar/Navbar.jsx'
 
 export default function Header () {
+
+
     return (
         <header className={styles.header}>
-            <img className={styles.img}  src="src/assets//coin_icon.png" />
+            <div className={styles.logoContainer}>
+                <img className={styles.img}  src="src/assets//coin_icon.png" />
+                <p className={styles.logoText}>E.R. Mariesdotter</p>
+            </div>
             <Navbar />
         </header>
     )
