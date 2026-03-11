a85fed7fbb314f14727f34cbfacaedb0681f0873
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 15:58:00 +0100

    Add styling and proper styling props to Experience Article component

--- src/components/ExperienceArticle/ExperienceArticle.jsx
+++ src/components/ExperienceArticle/ExperienceArticle.jsx
@@ -2,24 +2,24 @@ import styles from './ExperienceArticle.module.css';
 
 export default function ExperienceArticle ({timeline, title, subtitle, text}) {
     return (
-        <>
+        <article className={styles.article}>
         <div className={styles.experienceGraph}>
             <p>
                 {timeline}
             </p>
-            <div></div>
+            <div className={styles.line}></div>
         </div>
         <div>
-            <h2>
+            <h2 className={styles.h2}>
                 {title}
             </h2>
-            <h3>
+            <h3 className={styles.h3}>
                 {subtitle}
             </h3>
-            <p>
+            <p className={styles.p}>
                 {text}
             </p>
         </div>
-        </>
+        </article>
     )
 }
