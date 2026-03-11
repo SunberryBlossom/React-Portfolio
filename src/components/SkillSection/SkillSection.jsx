cd1b94371e87de99e5d7a2ff4ca9e6ab6eb6b239
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-11 14:12:28 +0100

    Add fontawesome icons and styling

--- src/components/SkillSection/SkillSection.jsx
+++ src/components/SkillSection/SkillSection.jsx
@@ -3,19 +3,19 @@ import SkillCard from '../SkillCard/SkillCard';
 
 export default function SkillSection () {
     return (
-        <section className={styles.SkillSection}>
-            <h2>My Current Skillset</h2>
-            <div>
-                <SkillCard skillName="HTML 5" />
-                <SkillCard skillName="CSS 3" />
-                <SkillCard skillName="JS" />
-                <SkillCard skillName="React" />
-                <SkillCard skillName="C# | .NET" />
-                <SkillCard skillName="MS SQL Server" />
-                <SkillCard skillName="PostGreSQL" />
-                <SkillCard skillName="Docker" />
-                <SkillCard skillName="Git VCS" />
-                <SkillCard skillName="REST API" />
+        <section className={styles.section}>
+            <h2 className={styles.h2}>My Current Skillset</h2>
+            <div className={styles.container}>
+                <SkillCard skillName="HTML 5" icon={['fab', 'html5']} iconStyle={{ color: 'rgb(221, 75, 37)' }} />
+                <SkillCard skillName="CSS 3" icon={['fab', 'css3']} iconStyle={{ color: 'rgb(53, 149, 207)' }} />
+                <SkillCard skillName="JS" icon={['fab', 'square-js']} iconStyle={{ color: 'rgb(239, 216, 29)' }} />
+                <SkillCard skillName="React" icon={['fab', 'react']} iconStyle={{ color: 'rgb(94, 211, 243)' }} />
+                <SkillCard skillName="C# | .NET" icon={['fas', 'code']} iconStyle={{ color: 'rgb(167, 108, 225)' }} />
+                <SkillCard skillName="MS SQL Server" icon={['fas', 'database']} iconStyle={{ color: 'rgb(49, 100, 140)' }} />
+                <SkillCard skillName="PostGreSQL" icon={['fas', 'database']} iconStyle={{ color: 'rgb(49, 100, 140)' }} />
+                <SkillCard skillName="Docker" icon={['fab', 'docker']} iconStyle={{ color: 'rgb(19, 83, 210)' }} />
+                <SkillCard skillName="Git VCS" icon={['fab', 'git-alt']} iconStyle={{ color: 'rgb(232, 77, 49)' }} />
+                <SkillCard skillName="REST API" icon={['fas', 'server']} iconStyle={{ color: 'rgb(95, 94, 94)' }} />
             </div>
         </section>
     )
