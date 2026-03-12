7687f9b36e9b7ba8101cf94e4c7b0b08150eb454
Vira R. Mariesdotter <wooden_anemone@pm.me>
2026-03-12 09:39:40 +0100

    Add resume section to resume page component

--- src/components/ResumePage/ResumePage.jsx
+++ src/components/ResumePage/ResumePage.jsx
@@ -1,52 +1,88 @@
 import Header from "../Header/Header";
 import Button from "../Button/Button";
 import Footer from "../Footer/Footer";
+import Resume from "./resume.json";
 
 export default function ResumePage () {
+    const data = Resume.english;
+
     return (
         <>
             <Header />
             <section>
                 <h1>
-                    Resume
+                    {data.profile.title == "profile" ? "Resume" : "CV"}
                 </h1>
-                <img />
                 <article>
+                    <img src="src/assets/resume-image.jpeg" />
                     <h2>
-                        Profile
+                        {data.header.name}
                     </h2>
+                    <h3>
+                        {data.header.subtitle}
+                    </h3>
+                    <p>
+                        {
+                            data.header["personal info"]
+                            .map( v => `${v} | ` )
+                        }
+                    </p>
                 </article>
                 <article>
                     <h2>
-                        Education
+                        {data.profile.title}
                     </h2>
+                    <p>
+                        {data.profile.text}
+                    </p>
                 </article>
                 <article>
                     <h2>
-                        Skills
+                        education
                     </h2>
+                    <div>
+                        {data.education.map((education, index) => (
+                            <div key={index}>
+                                <h3>
+                                    {education.title}
+                                </h3>
+                                <h4>
+                                    {education.subtitle}
+                                </h4>
+                                <p>
+                                    {education.period}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
                 </article>
                 <article>
                     <h2>
-                        Work Experience
+                        work experience
                     </h2>
+                    <div>
+                        {data["work experience"].map((work, index) => (
+                            <div key={index}>
+                                <h3>
+                                    {work.title}
+                                </h3>
+                                <h4>
+                                    {work.subtitle}
+                                </h4>
+                                <p>
+                                    {work.period}
+                                </p>
+                                <ul>
+                                    {work.text.map((text, index) => (
+                                        <li key={index}>
+                                            {text}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
                 </article>
-            </section>
-            <section>
-                <div>
-                    <h2>
-                        ATS-friendly resumes
-                    </h2>
-                    <Button />
-                    <Button />
-                </div>
-            </section>
-            <section>
-                <h2>
-                    Want to start something great?
-                </h2>
-                <Button />
-                <Button />
             </section>
             <Footer />
         </>
